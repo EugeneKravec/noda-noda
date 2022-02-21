544e41476b9da729983a94105ed87f1497bc16d6
Eugene Kravec <krava255@gmail.com>
2022-02-21 17:51:46 +0200

    Continue practice

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { json } = require("express/lib/response");
 const fs = require("fs/promises");
+const { json } = require("express/lib/response");
 const path = require("path")
 
 const filePath = path.normalize(__dirname + '/db/users.json')
+
 
 const app = express();
 app.listen(5000, () => console.log('Serever is runnig on the port 5000'));
@@ -22,14 +23,21 @@ app.get('/users/:id', async (req, res) => {
         const { id } = req.params;
         const users = JSON.parse(await fs.readFile(filePath));
 
-        const user = users.filter((user) => user.id === id);
+        const user = users.filter((user) => String(user.id) === id);
         res.json(user);
     } catch (e) {
         res.json({ error: e.message });
     }
 })
 
-// app.post('/users', (req, res) => { })
+app.post('/users', (req, res) => {
+    try {
+        const users = JSON.parse(await fs.readFile(filePath));
+        const body = req.body;
+    } catch (e) {
+
+    }
+})
 // app.put('/users/:id', (req, res) => { })
 
 // app.delete('/users/:id', (req, res) => { })
